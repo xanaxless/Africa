b7cc4cd5f9b88389e49632322c220306c934ee49
Yerkebulan Serikov <serikov.2003.18@gmail.com>
2022-04-22 15:43:24 +0600

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Yerkebulan Serikov on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
